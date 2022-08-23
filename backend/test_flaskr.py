f8492d95a183f9d9ad7c0b3eed3f963854f648f1
marzouk IDRISSOU <islamsunnaestlaveriter@gmail.com>
2022-08-23 16:47:41 +0200

    updating

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,7 +15,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgresql://anged@{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgresql://postgres@{}/{}".format('localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +24,6 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-
 
         self.new_question = {
             'question': 'new question',
@@ -79,22 +78,39 @@ class TriviaTestCase(unittest.TestCase):
     #     """
     #     test de suppression selon l'id envoye en parametre
     #     """
-    #     response = self.client().delete('/api/questions/2')
-    #     data = json.loads(response.data)
+    #     res = self.client().delete('/api/questions/2')
+    #     data = json.loads(res.data)
 
-    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(res.status_code, 422)
     #     self.assertEqual(data['success'], True)
-    #     self.assertEqual(data['deleted'],2)
+    #     self.assertEqual(data['deleted'], 2)
+
+    # def test_delete_question(self):
+    #     question = Question(question='new question', answer='new answer',
+    #                         difficulty=1, category=1)
+    #     question.insert()
+    #     question_id = question.id
+
+    #     res = self.client().delete(f'/api/questions/{question_id}')
+    #     data = json.loads(res.data)
+
+    #     question = Question.query.filter(
+    #         Question.id == question.id).one_or_none()
+
+    #     self.assertEqual(res.status_code, 200)
+    #     self.assertEqual(data['success'], True)
+    #     self.assertEqual(data['deleted'], int(question_id))
+    #     self.assertEqual(question, None)
     
 
-    # def test_delete_question_404(self):   
-    #     """
-    #     test de suppression avec un mauvais id
-    #     """
-    #     response = self.client().delete('/api/questions/1000')
-    #     data = json.loads(response.data)
+    def test_delete_question_422(self):   
+        """
+        test de suppression avec un mauvais id
+        """
+        response = self.client().delete('/api/questions/1000')
+        data = json.loads(response.data)
 
-    #     self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 422)
 
     
     
@@ -109,7 +125,7 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
     
     
-    def test_405_question_creation_not_allowed(self):
+    def test_405_question(self):
         """
         test de l'erreur 405
         """
@@ -134,37 +150,37 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['success'], True)
 
     
-    # def test_get_404_questions_by_category(self):
-    #     """
-    #     test de recuperation d'une ressource qui n'existe pas
-    #     """
-    #     response = self.client().get('/api/categories/1000/questions')
-    #     data = json.loads(response.data)
+    def test_get_404_questions_by_category(self):
+        """
+        test de recuperation d'une ressource qui n'existe pas
+        """
+        response = self.client().get('/api/categories/1000/questions')
+        data = json.loads(response.data)
 
-    #     self.assertEqual(response.status_code, 404)
-    #     self.assertEqual(data['message'], 'resource not found')
-    #     self.assertEqual(data['success'], False)
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['message'], 'resource not found')
+        self.assertEqual(data['success'], False)
+
+    
+    def test_play_quiz(self):
+        new_quiz = {'previous_questions': [],
+                            'quiz_category': {'type': 'Entertainment', 'id': 5}}
+
+        res = self.client().post('/api/quizzes', json=new_quiz)
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
     
     
-    # def test_play_quiz_1(self):
-    #     """
-    #     test de creation de jeux
-    #     """
-    #     res = self.client().post('/api/quizzes', json={"prev": [], "quiz_category": {"type": "Geography", "id": "3"}})
-    #     data = json.loads(res.data)
-    #     self.assertEqual(data['success'], True)                 
-    #     self.assertIsNotNone(data['question'])                  
-    #     self.assertEqual(data['question']['category'], 3) 
+    def test_404_play_quiz(self):
+        new_quiz = {'previous_questions': []}
+        res = self.client().post('/api/quizzes', json=new_quiz)
+        data = json.loads(res.data)
 
-    # def test_play_quiz_4(self):
-    #     """
-    #     test error messages
-    #     """
-    #     res = self.client().post('/api/quizzes', json={"prev": [13], "quiz_category": {"type": "Geography"}})
-    #     data = json.loads(res.data)
-    #     self.assertEqual(data['success'], False)                 
-    #     self.assertEqual(data['error'], 400)   
-
+        self.assertEqual(res.status_code, 422)
+        self.assertEqual(data["success"], False)
+        self.assertEqual(data["message"], "unprocessable")
 
 
 
